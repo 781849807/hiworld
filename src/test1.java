2ca15f624400aac228ca42c18aeb0c37490e6a94
zhaoyuan <781849807@qq.com>
2021-11-09 14:41:24 +0800

    再来一次

--- src/test1.java
+++ src/test1.java
@@ -1,5 +1,6 @@
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("hi git测试一次");
+        System.out.println("hi git");
+        System.out.println("再来一次");
     }
 }
